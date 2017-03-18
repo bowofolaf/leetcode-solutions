6f00b9caca627e5ef215d6c666f078dba1deb571
Bowofola <bowofola@gmail.com>
2017-03-18 16:47:48 -0400

    Create lc27-remove-element.js

---
+++ lc27-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var i = 0, j = 0;
+    
+    while(j < nums.length) {
+        if(nums[j] != val){
+            nums[i++] = nums[j];
+        }
+        j++;
+    }
+    
+    return i;
+};
