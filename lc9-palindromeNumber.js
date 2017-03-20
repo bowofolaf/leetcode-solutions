bfa3549ab9da1236aa22d3bc41138f241f695ba2
Bowofola <bowofola@gmail.com>
2017-03-20 16:45:08 -0400

    Update lc9-palindromeNumber.js

--- lc9-palindromeNumber.js
+++ lc9-palindromeNumber.js
@@ -3,8 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(result > 2147483647) return false;// max int
-
     var result = 0, y = x;
     while(y > 0) {
         var digit = y % 10;
